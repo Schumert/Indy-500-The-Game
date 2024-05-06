4613ac00795299d11d5f63569403b6df3021fd44
Schumert <scaramouche.fandango_70@hotmail.com>
2024-05-07 00:46:45 +0300

    I FUCKING DID IT
    I FUCKING DID THE BOUNCY STUFF WITH THE CHARACTERBODY2D AND NOT THE FUCKING RIGIDBODY

--- scripts/CarMovement.gd
+++ scripts/CarMovement.gd
@@ -1,98 +1,87 @@
 extends CharacterBody2D
 
-var car_heading
-var steer_angle = 12
-var steer_angle_fast = 7
-var steer_angle_speed_bound = 2000 # type of: vel. length
+
+var steer_angle = 15
+var steer_angle_fast = 12
+var steer_angle_very_fast = 8
 var acceleration = Vector2.ZERO
 var friction = -55
 var drag = -0.06
 var engine_power = 0
 var wheel_base = 70
 var braking = -450
-var max_speed_reverse = 250
-var slip_speed = 2000 #type of: vel. length
+var max_speed_reverse = 750
+var slip_speed = 1500 #type of: vel. length
 var traction_fast = 3
 var traction_slow = 8
+
+var is_pushing_state:=false
+
 # var traction_handbrake = 0
 
 # var handbraking_fast = -50
 # var handbrake_speed = 100
 # var handbrake:=false
 
-var traction = traction_slow
-var is_sliding:= false
+
 
 
 var steer_direction
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
 
+var collision_info = Vector2.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	acceleration = Vector2.ZERO
-	get_input(delta)
+	get_input()
 	apply_friction(delta)
 	steering(delta)
 	velocity += acceleration * delta
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().name == "Area2D":
+			is_pushing_state = true
+			collision_info = collision.get_normal()
+	_pushed_off(collision_info)
 
-	#print(velocity.length())
-	print(steer_angle)
+
+			
 	
+	#print(velocity.length())
+	#print(steer_angle)
+	
+	
+func _pushed_off(opp:Vector2):
+	if is_pushing_state:
+		acceleration = Vector2.ZERO
 
-var old_steer_angle = steer_angle
-func get_input(delta):
+		velocity += global_position.direction_to(opp) * 1000
+		await get_tree().create_timer(0.5).timeout
+		engine_power = 0
+
+		is_pushing_state = false
+func get_input():
 	var turn = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
 
 	steer_direction = turn * deg_to_rad(steer_angle)
+	
+	acceleration = transform.x * engine_power
 	if Input.is_action_pressed("accelerate"):
-		engine_power+=30
-		if engine_power >= 7500: #engine_power=5000 -> vel.length=1824   engine_power=10.000 -> vel.length=2736
-			engine_power = 7500
+		engine_power = lerpf(engine_power, 7500, 0.005)
 		
 	else:
-		engine_power -= 30
-		if engine_power < 0:
-			engine_power = 0
+		engine_power = lerpf(engine_power, 0, 0.01)
 
+	
+	
 	if Input.is_action_pressed("brake"):
-		engine_power -= 100
-		if engine_power <= 0:
-			engine_power = 0
-			acceleration = transform.x * braking
-	else:
-		acceleration = transform.x * engine_power
+		engine_power = lerpf(engine_power, 0, 0.1)
+		acceleration = transform.x * braking
 
-	
-	#handbrake but car's sliding rather than slowing, it is slowing but very little.
-	#Handbrake speed is necessary because without it, car's behaving weird after pressing long enough the handbrake button. 
-	# if Input.is_action_pressed("handbrake") and velocity.length() > handbrake_speed:
-	# 	handbrake = true
-	# 	is_sliding = true
-
-	# 	#velocity = lerp(velocity, Vector2.ZERO, 0.02)#Makes you stop faster than normal break if you handbrake without acceleration.
-
-	# elif Input.is_action_pressed("handbrake"):
-	# 	handbrake = true
-	# 	is_sliding = false
-	# 	#velocity = Vector2.ZERO
-	# 	velocity = lerp(velocity, Vector2.ZERO, 0.2) #And eventually, car stops.
-
-	
-	
-	# if Input.is_action_just_released("handbrake") and velocity.length() > handbrake_speed:
-	# 	#await get_tree().create_timer(0.5).timeout
-	# 	is_sliding = false
-	# 	handbrake = false
-	# elif Input.is_action_just_released("handbrake"):
-	# 	is_sliding = false
-	# 	handbrake = false
-
-
+var old_steer_angle = steer_angle
 func steering(delta):
 	
 	var front_wheel = position + transform.x * wheel_base / 2
@@ -107,30 +96,23 @@ func steering(delta):
 	#print(atan2(front_wheel.y - back_wheel.y, front_wheel.x - back_wheel.x))
 	
 	#car_heading = (front_wheel - back_wheel).normalized()
-	car_heading = back_wheel.direction_to(front_wheel)
+	var car_heading = back_wheel.direction_to(front_wheel)
 
-	
-	
-	
-	if velocity.length() > steer_angle_speed_bound:
-		steer_angle -= 0.05
-		if steer_angle <= steer_angle_fast:
-			steer_angle = steer_angle_fast
-	else:
-		steer_angle += 0.05
-		if steer_angle >= old_steer_angle:
-			steer_angle = old_steer_angle
-
-
-
-	traction = traction_slow
-	if velocity.length() > slip_speed and not is_sliding:
+	var steer_angle_speed_bound = 1500 # type of: vel. length
+	var traction = traction_slow
+	if velocity.length() > steer_angle_speed_bound + 500:
+		steer_angle = steer_angle_very_fast
+		traction = traction_fast - 2
+	elif velocity.length() > steer_angle_speed_bound:
+		steer_angle = steer_angle_fast
 		traction = traction_fast
+	else:
+		steer_angle = old_steer_angle
 
-	# if handbrake:
-	# 	traction = traction_handbrake
 	
+		
 
+	
 	var d = car_heading.dot(velocity.normalized())
 	if d > 0:	
 		velocity = lerp(velocity, car_heading * velocity.length(), traction * delta)
@@ -140,11 +122,9 @@ func steering(delta):
 	rotation = car_heading.angle()
 
 func apply_friction(delta):
-	if acceleration == Vector2.ZERO and velocity.length() < 70:
+	if  acceleration == Vector2.ZERO and velocity.length() < 50:
 		velocity = Vector2.ZERO
 	
 	var friction_force = velocity * friction * delta
 	var drag_force = velocity * velocity.length() * drag * delta
-	if velocity.length() < 100:
-		friction_force *= 3
 	acceleration += friction_force + drag_force
